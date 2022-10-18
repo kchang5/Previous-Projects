39f5d08798e050401b26f00446b55cc72f0babe8
kchang5 <kevin.chang3918@gmail.com>
2022-10-18 12:00:22 -0700

    Update IntVector.cpp

--- CS10B/lab 6/IntVector.cpp
+++ CS10B/lab 6/IntVector.cpp
@@ -4,11 +4,16 @@
 
 using namespace std;
 
+//This function should set both the size and capacity of the IntVector to 0 and will not allocate any memory to store integers.
+//(Make sure you do not have a dangling pointer.)
+
 IntVector::IntVector(){
     _size = 0;
     _capacity = 0;
     _data = nullptr;
 }
+// Sets both the size and capacity of the IntVector to the value of the parameter passed in and dynamically allocates an array of that size as well. 
+//This function should initialize all elements of the array to the value of the 2nd parameter.
 
 IntVector::IntVector(unsigned size, int value){
     _size = size;
@@ -19,24 +24,32 @@ IntVector::IntVector(unsigned size, int value){
         _data[i] = value;
     }
 }
+//The destructor is used to clean up (delete) any remaining dynamically-allocated memory. 
+//For this class, that will be the array pointed to by the int pointer called data.
 
 IntVector::~IntVector(){
     delete _data;
     _data = nullptr;
 }
 
+//This function returns the current size (not the capacity) of the IntVector object, which is the values stored in the sz data field.
 unsigned IntVector::size() const{
     return _size;
 }
 
+//This function returns the current capacity (not the size) of the IntVector object, which is the value stored in the cap data field.
 unsigned IntVector::capacity() const{
     return _capacity;
 }
 
+//Returns whether the IntVector is empty (the sz field is 0).
 bool IntVector::empty() const{
     return _size == 0;
 }
 
+//This function will return the value stored in the element at the passed in index position. 
+//Your function should throw an outofrange exception if an invalid index is passed in. 
+//An invalid index is an index greater than or equal to the size
 const int & IntVector::at(unsigned index) const{
     if (empty() || index < 0 || index >= _size){
         throw out_of_range("IntVector::at_range_check");
@@ -45,13 +58,16 @@ const int & IntVector::at(unsigned index) const{
     return _data[index];
 }
 
+//This function will return the value stored in the first element.
 const int & IntVector::front() const{
     return _data[0];
 }
 
+//This function will return the value stored in the last element.
 const int & IntVector::back() const{
     return _data[_size - 1];
 }
+
 
 void IntVector::assign(unsigned n, int value)
 {
